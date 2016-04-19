8013527c2a8396382def692bd49ef55317bf908e
Espen Wiborg <eswibo@basefarm.no>
2016-04-19 09:05:07 +0200

    Initial Commit

---
+++ AutoClicker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AutoClicker
+//
+//  Created by Espen Wiborg on 2016-04-19.
+//  Copyright © 2016 Espen Wiborg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
