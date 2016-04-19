c81688fb099d7997fc931bc4e3f2fdfcbb59e37f
Espen Wiborg <espenhw@grumblesmurf.org>
2016-04-19 16:09:46 +0200

    Initial working version

--- AutoClicker/AppDelegate.swift
+++ AutoClicker/AppDelegate.swift
@@ -6,22 +6,92 @@
 //  Copyright © 2016 Espen Wiborg. All rights reserved.
 //
 
-import Cocoa
+import MASShortcut
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var preferencesWindow: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var shortcutView: MASShortcutView!
 
+    @IBOutlet weak var intervalLabel: NSTextField!
+    @IBOutlet weak var intervalSlider: NSSlider!
+    
+    var prefsActive: Bool = false
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    let shortcutKey = "GlobalShortcut"
+    let intervalKey = "Interval"
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+
+    @IBAction func intervalChanged(sender: NSSlider) {
+        intervalLabel.stringValue = String(sender.intValue)
+        defaults.setInteger(sender.integerValue, forKey: intervalKey)
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+    
+    @IBAction func preferencesClicked(sender: NSMenuItem) {
+        preferencesWindow.makeKeyAndOrderFront(self)
+        NSApp.activateIgnoringOtherApps(true)
+        prefsActive = true
+    }
+    
+    func windowWillClose(notification: NSNotification) {
+        prefsActive = false
+    }
+    
+    func callback() {
+        let skip = prefsActive && preferencesWindow.frame.contains(NSEvent.mouseLocation())
+        
+        if !skip {
+            toggleClicking()
+        }
+    }
+    
+    var timer: NSTimer? = nil
+    
+    func toggleClicking() {
+        if timer == nil {
+            timer = NSTimer.scheduledTimerWithTimeInterval(intervalSlider.doubleValue / 1000, target: self, selector: Selector("click"), userInfo: nil, repeats: true)
+        } else {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    func click() {
+        let dummy = CGEventCreate(nil)
+        let loc = CGEventGetLocation(dummy)
+        
+        let mouseDown = CGEventCreateMouseEvent(nil, CGEventType.LeftMouseDown, loc, CGMouseButton.Left)
+        let mouseUp = CGEventCreateMouseEvent(nil, CGEventType.LeftMouseUp, loc, CGMouseButton.Left)
+        CGEventPost(CGEventTapLocation.CGHIDEventTap, mouseDown)
+        CGEventPost(CGEventTapLocation.CGHIDEventTap, mouseUp)
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem.title = "AutoClicker"
+        statusItem.menu = statusMenu
+        shortcutView.associatedUserDefaultsKey = shortcutKey
+        
+        
+        
+        intervalSlider.integerValue = max(defaults.integerForKey(intervalKey), Int(intervalSlider.minValue))
+        intervalChanged(intervalSlider)
+        
+        preferencesWindow.delegate = self
+        preferencesWindow.level = Int(CGWindowLevelForKey(.FloatingWindowLevelKey))
+        
+        MASShortcutBinder.sharedBinder().bindShortcutWithDefaultsKey(shortcutKey, toAction: callback)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        MASShortcutMonitor.sharedMonitor().unregisterAllShortcuts()
     }
-
-
 }
 
---
+++ Swift_bridge.h
@@ -0,0 +1,15 @@
+//
+//  Swift_bridge.h
+//  AutoClicker
+//
+//  Created by Espen Wiborg on 2016-04-19.
+//  Copyright © 2016 Espen Wiborg. All rights reserved.
+//
+
+#ifndef Swift_bridge_h
+#define Swift_bridge_h
+
+#import <Cocoa/Cocoa.h>
+#import <MASShortcut/Shortcut.h>
+
+#endif /* Swift_bridge_h */
